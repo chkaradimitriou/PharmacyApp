844fbefa23ab027acd616f3945387844335de2b4
chkaradimitriou <chkaradimitriou@gmail.com>
2024-08-31 14:14:07 +0300

    project init

---
+++ app/src/main/java/com/ckaradimitriou/pharmacyapp/ui/splash/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.ckaradimitriou.pharmacyapp.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ckaradimitriou.pharmacyapp.databinding.ActivitySplashBinding;
+import com.ckaradimitriou.pharmacyapp.ui.login.LoginActivity;
+import com.ckaradimitriou.pharmacyapp.ui.register.RegisterActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        binding.loginBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        binding.registerBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+}
